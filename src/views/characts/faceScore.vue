41836d6434792c7ed6a73b1dcd07c5d166532243
唐娟 <tangjuan@tangjuandeMacBook-Pro.local>
2017-09-20 17:47:29 +0800

    develop

---
+++ src/views/characts/faceScore.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+        <singleOneLine :mes="message"></singleOneLine>
+    </div>
+</template>
+<script>
+import singleOneLine from '../../components/singleOneLine'
+export default {
+    components: { singleOneLine },
+    data() {
+        return {
+            message: {
+                tit: '您左手呈现什么样的姿势呢',
+                dataList: [
+                    {
+                        key: 0,
+                        name: '',
+                        src: '8-1.png',
+                        choiced: false
+                    },
+                    {
+                        key: 1,
+                        name: '',
+                        src: '8-2.png',
+                        choiced: false
+                    },
+                   
+                ],
+                page: 1,
+                imgPackage: 'characts',
+                pageName:'clock',
+                nextPage:'./'
+            }
+
+        }
+    },
+  
+}
+</script>
+
+
