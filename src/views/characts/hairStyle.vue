09765e92d53b757bb7e212186775d117c90a148f
afacode <afacode@outlook.com>
2017-09-21 09:19:34 +0800

    lifaq push

---
+++ src/views/characts/hairStyle.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+        <singleOneLine :mes="message"></singleOneLine>
+    </div>
+</template>
+<script>
+import singleOneLine from '../../components/singleOneLine'
+export default {
+    components: { singleOneLine },
+    data() {
+        return {
+            message: {
+                tit: '你喜欢的发型是',
+                dataList: [
+                    {
+                        key: 0,
+                        name: '清爽干练',
+                        src: '9-1.png',
+                        choiced: false
+                    },
+                    {
+                        key: 1,
+                        name: '花样帅气',
+                        src: '9-2.png',
+                        choiced: false
+                    },
+                   
+                ],
+                page: 1,
+                imgPackage: 'characts',
+                pageName:'shoes',
+                nextPage:'./toothpaste'
+            }
+
+        }
+    },
+  
+}
+</script>
+
+
---
+++ src/views/characts/shoes.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+        <singleOneLine :mes="message"></singleOneLine>
+    </div>
+</template>
+<script>
+import singleOneLine from '../../components/singleOneLine'
+export default {
+    components: { singleOneLine },
+    data() {
+        return {
+            message: {
+                tit: '约会一般你会穿什么样的鞋',
+                dataList: [
+                    {
+                        key: 0,
+                        name: '高跟鞋',
+                        src: '10-1.png',
+                        choiced: false
+                    },
+                    {
+                        key: 1,
+                        name: '平底鞋',
+                        src: '10-2.png',
+                        choiced: false
+                    },
+                   
+                ],
+                page: 1,
+                imgPackage: 'characts',
+                pageName:'shoes',
+                nextPage:'./toothpaste'
+            }
+
+        }
+    },
+  
+}
+</script>
+
+
