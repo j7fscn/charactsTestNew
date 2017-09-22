13607e2ebee7971dbde941d800193352f139b802
afacode <afacode@outlook.com>
2017-09-22 11:40:47 +0800

    lifaq 图片添加

--- src/views/characts/choiceDraw.vue
+++ src/views/characts/choiceDraw.vue
@@ -15,28 +15,28 @@ export default {
                     {
                         key: 0,
                         name: '',
-                        src: '13-1.png',
+                        src: '19-1.png',
                         choiced: false,
                         score:4,
                     },
                     {
                         key: 1,
                         name: '',
-                        src: '13-2.png',
+                        src: '19-2.png',
                         choiced: false,
                         score:3
                     },
                     {
                         key: 2,
                         name: '',
-                        src: '13-3.png',
+                        src: '19-3.png',
                         choiced: false,
                         score:4
                     },
                     {
                         key: 3,
                         name: '',
-                        src: '13-4-1png',
+                        src: '19-4.png',
                         choiced: false,
                         score:4
                     }
