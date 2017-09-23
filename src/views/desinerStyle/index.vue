a60453d2d561e8a2f6f04829cf86bc8fe0419ed4
唐娟 <tangjuan@tangjuandeMacBook-Pro.local>
2017-09-23 16:18:16 +0800

    你喜欢的风格

--- src/views/desinerStyle/index.vue
+++ src/views/desinerStyle/index.vue
@@ -10,7 +10,7 @@
         <p class="tit">选择您喜欢的风格</p>
         <div class="imgWrap">
             <transition name="fade">
-                <img v-lazy="'static/images/desinerStyle/f-'+imgOrder+'.png'">
+                <img v-lazy="'static/images/desinerStyle/f-'+imgOrder+'.png'" :class="{imgAimate:fadeOut}">
             </transition>
         </div>
         <div class="opacity">
@@ -35,22 +35,27 @@
 export default {
     data() {
         return {
+            fadeOut:false,
             canClick: true,
             dislike: false,
             nofelling: false,
             like: false,
-            northernEurope: 0,//北欧
-            mediterraneanSea: 0,//地中海
-            french: 0,//法式
-            industrialWind: 0,//工业风
-            europeanism: 0,//古典欧式
-            beautifulSimplicity: 0,//简美
-            janeEuropean: 0,//简欧
-            lightLuxury: 0,//轻奢
-            japanese: 0,//日式
-            modern: 0,//现代
-            countryStyle: 0,//乡村美式
-            chineseStyle: 0,//中式
+            fisrtStyle: '',
+            secondStyle: '',
+            styleScore: {
+                northernEurope: 0,//北欧
+                mediterraneanSea: 0,//地中海
+                french: 0,//法式
+                industrialWind: 0,//工业风
+                europeanism: 0,//古典欧式
+                beautifulSimplicity: 0,//简美
+                janeEuropean: 0,//简欧
+                lightLuxury: 0,//轻奢
+                japanese: 0,//日式
+                modern: 0,//现代
+                countryStyle: 0,//乡村美式
+                chineseStyle: 0,//中式
+            },
             imgOrder: 1,
             styleList: [
                 {
@@ -78,34 +83,166 @@ export default {
                         { name: 'french', score: 1 }
                     ]
                 },
-                  {
+                {
                     classFily: [
                         { name: 'french', score: 1 }
                     ]
-                }
+                },
+                {
+                    classFily: [
+                        { name: 'industrialWind', score: 1 }
+                    ]
+                },
+                {
+                    classFily: [
+                        { name: 'industrialWind', score: 1 }
+                    ]
+                },
+                {
+                    classFily: [
+                        { name: 'europeanism', score: 1 }
+                    ]
+                },
+                {
+                    classFily: [
+                        { name: 'europeanism', score: 1 }
+                    ]
+                },
+                {
+                    classFily: [
+                        { name: 'beautifulSimplicity', score: 1 }
+                    ]
+                },
+                {
+                    classFily: [
+                        { name: 'beautifulSimplicity', score: 1 }
+                    ]
+                },
+                {
+                    classFily: [
+                        { name: 'janeEuropean', score: 1 }
+                    ]
+                },
+                {
+                    classFily: [
+                        { name: 'janeEuropean', score: 1 }
+                    ]
+                },
+                {
+                    classFily: [
+                        { name: 'lightLuxury', score: 1 }
+                    ]
+                },
+                {
+                    classFily: [
+                        { name: 'lightLuxury', score: 1 }
+                    ]
+                },
+                {
+                    classFily: [
+                        { name: 'japanese', score: 1 }
+                    ]
+                },
+                {
+                    classFily: [
+                        { name: 'japanese', score: 1 }
+                    ]
+                },
+                {
+                    classFily: [
+                        { name: 'modern', score: 1 }
+                    ]
+                },
+                {
+                    classFily: [
+                        { name: 'modern', score: 1 }
+                    ]
+                },
+                {
+                    classFily: [
+                        { name: 'countryStyle', score: 1 }
+                    ]
+                },
+                {
+                    classFily: [
+                        { name: 'countryStyle', score: 1 }
+                    ]
+                },
+                {
+                    classFily: [
+                        { name: 'chineseStyle', score: 1 }
+                    ]
+                },
+                {
+                    classFily: [
+                        { name: 'mediterraneanSea', score: .11 },
+                        { name: 'countryStyle', score: .55 },
+                        { name: 'beautifulSimplicity', score: .34 }
+                    ]
+                },
+                {
+                    classFily: [
+                        { name: 'french', score: .94 },
+                        { name: 'lightLuxury', score: .2 },
+                    ]
+                },
+                {
+                    classFily: [
+                        { name: 'northernEurope', score: .17 },
+                        { name: 'lightLuxury', score: .57 },
+                        { name: 'europeanism', score: .9 }
+                    ]
+                },
+                {
+                    classFily: [
+                        { name: 'lightLuxury', score: .6 },
+                        { name: 'mediterraneanSea', score: .27 },
+                        { name: 'europeanism', score: .13 }
+                    ]
+                },
+                {
+                    classFily: [
+                        { name: 'modern', score: .65 },
+                        { name: 'chineseStyle', score: .35 },
+
+                    ]
+                },
+                {
+                    classFily: [
+                        { name: 'beautifulSimplicity', score: .8 },
+                        { name: 'lightLuxury', score: .15 },
+                        { name: 'northernEurope', score: .05 }
+                    ]
+                },
+                {
+                    classFily: [
+                        { name: 'japanese', score: 1 }
+                    ]
+                },
+
             ]
 
         }
     },
     methods: {
         likeChoice() {
-             this.addScore(1);
+            this.addScore(1);
             this.choiceOption('like');
-           
+
         },
         dislikeChoice() {
             this.addScore(0);
             this.choiceOption('dislike');
-            
+
         },
         nofellingChoice() {
-             this.addScore(0.05);
+            this.addScore(0.05);
             this.choiceOption('nofelling');
-           
+
 
         },
         choiceOption(btn) {
-            var _self=this;
+
             if (!this.canClick) {
                 return
             }
@@ -117,35 +254,65 @@ export default {
             } else {
                 this.nofelling = true;
             }
+            this.fadeOut =false;
+            if (this.imgOrder >= 30) {
+                this.like = false;
+                this.dislike = false;
+                this.nofelling = false;
+                this.maxScore();
+                this.fadeOut =true;
+                return
+
+            }
+
             this.imgOrder += 1;
             this.canClick = false;
-            this.imgOrder += 1;
-            setTimeout(function(){
-                _self.canClick = true;
-                _self.like = false;
-                _self.dislike = false;
-                _self.nofelling = false;
+            setTimeout(() => {
 
-            }, 200);
+                this.like = false;
+                this.dislike = false;
+                this.nofelling = false;
+                this.canClick = true;
+
+            }, 500);
+            setTimeout(() => {
+                this.fadeOut = true;
+            }, 50);
         },
         addScore(percent) {
-         
-            var items=this.styleList[this.imgOrder-1].classFily;
-             
-            var score=0;
-            items.forEach((element)=> {
-                this[element.name]+=element.score*percent;
+            var items = this.styleList[this.imgOrder - 1].classFily;
+            items.forEach((element) => {
+                this.styleScore[element.name] += element.score * percent;
 
             });
-           
-            // for(var i=0;i<items.length;i++){
-              
-            //    _self[items[i].name]+=items[i][score]*percent;
-            //       debugger
-            // }
-    
-           
+        },
+        maxScore() {
+            var max = 0;
+            var maxstyle = '';
+            for (var o in this.styleScore) {
+                if (max < this.styleScore[o]) {
+                    max = this.styleScore[o];
+                    maxstyle = o;
+                }
+            }
+
+            this.fisrtStyle = maxstyle;
+            this.secondMax();
+        },
+        secondMax() {
+            var second = 0;
+            var secondStyle = '';
+            for (var o in this.styleScore) {
+
+                if (second < this.styleScore[o] && (o != this.fisrtStyle)) {
+                    second = this.styleScore[o];
+                    secondStyle = o;
+                }
+            }
+            this.secondStyle = secondStyle;
+            console.log(this.fisrtStyle, this.secondStyle);
         }
+
     }
 
 }
@@ -174,6 +341,7 @@ export default {
 
 .pageDesinerStyle .imgWrap img {
     width: 100%;
+    transition-duration: 2s
 }
 
 .pageDesinerStyle .ceil {
@@ -184,16 +352,27 @@ export default {
     width: 100%;
 }
 
-.fade-enter-active,
-.fade-leave-active {
-    transition: opacity .5s
+
+.imgAimate {
+
+    animation: myfirst .5s;
+  
 }
 
-.fade-enter,
-.fade-leave-to
-/* .fade-leave-active in below version 2.1.8 */
-
+@keyframes myfirst
 {
-    opacity: 0
+from { 
+     margin-left: 100%;
+   opacity: 0;
+   transform: rotateX(45deg);
+     transform: rotateY(45deg);
+   }
+to { 
+    width: 100%;
+    opacity: 1;
+       transform: rotateX(0deg);
+            transform: rotateY(0deg);
+    }
 }
+
 </style>
