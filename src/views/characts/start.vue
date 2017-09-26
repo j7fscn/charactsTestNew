e509552a999efcdf5a1aa58adba905b9846044f5
唐娟 <tangjuan@tangjuandeMacBook-Pro.local>
2017-09-26 19:44:07 +0800

    udpate

--- src/views/characts/start.vue
+++ src/views/characts/start.vue
@@ -21,18 +21,23 @@ export default {
             if (!this.$route.params.userid ) {
                 alert('请输入用户ID')
             }
-            var url = ('http://192.168.2.240:8999/personalityTest/getPersonalityTestResult?user_id=' + this.$route.params.userid)
+            var url = ('http://120.27.215.62:8999/personalityTest/getPersonalityTestResult?user_id=' + this.$route.params.userid)
             this.$jsonp(url).then(function(json) {
                 // 插入新用户
                 if (json.data.result == '数据库没有该userId的记录') {
-                    _self.insertUserid()
+                    _self.insertUserid();
+                    return
                 }
-                var json = _self.parseQueryString(json.data.result)
+                 var json = _self.parseQueryString(json.data.result);
+                 debugger
                 if (json.shakeSmart == '0') {
                     _self.$router.push({ path: '/shakeFirst/' + _self.$route.params.userid })
                     return
                 }
                 _self.$router.push({ path: json.nextKey + '/' + _self.$route.params.userid })
+                
+             
+               
 
             }).catch(err => {
                 console.log(err)
@@ -41,7 +46,8 @@ export default {
         insertUserid() {
             var _self = this
             var urlNew = 'http://120.27.215.62:8999/personalityTest/insertPersonalityTestResult?user_id=' + this.$route.params.userid + '&shakeSmart=0&result=0'
-            this.$jsonp(urlNew).then(function(json1){
+            this.$jsonp(urlNew).then(function(json){
+                debugger
                 _self.$router.push({ path: '/shakeFirst/' + _self.$route.params.userid })
                 return
             }).catch(err => {
